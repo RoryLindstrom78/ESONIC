306a946b9259decfcccfeaf9c18a0a87875fb767
Rory Lindstrom <rorylindstrom78@gmail.com>
2026-02-18 13:50:10 -0800

    Updated UDP Server docstring to describe preferred format of files sent

--- network/udp_receiver.py
+++ network/udp_receiver.py
@@ -7,6 +7,26 @@ import queue
 
 
 class UDPServer(asyncio.DatagramProtocol):
+    """
+        UDP Server running that will receive ESP32 data.
+        Data should be sent as JSON in the following format:
+
+        {
+            "id": 1,
+            "t": 1708140034.123,
+            "ax": 0.12,
+            "ay": -0.87,
+            "az": 9.81
+        }
+
+        Where:
+            id: Device ID (1-4)
+            t: timestamp
+            ax: x-axis acceleration
+            ay: y-axis acceleration
+            az: z-axis acceleration
+    """
+
     def datagram_received(self, data, addr):
         message = json.loads(data.decode())
         print(message)
