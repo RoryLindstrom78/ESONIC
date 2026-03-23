0b690a43185f215026493a0f54b511ce9a55f3ea
Leslie Dong <leslied4@uci.edu>
2026-03-22 21:02:47 -0700

    changing login

--- ESP-MPU/espsender/espsender.ino
+++ ESP-MPU/espsender/espsender.ino
@@ -6,8 +6,8 @@
 #include <ArduinoJson.h>
 
 //WIFI stuff
-const char* ssid = "Velocity Wi-Fi";  //ex: Velocity Wi-Fi
-const char* password = "zphwoksk";    //enter password, here's bad security practice aka my password :)
+const char* ssid = "UCI-Guest";  //ex: Velocity Wi-Fi
+const char* password = "";    //enter password, here's bad security practice aka my password :)
 
 const char* udpAddress = "10.7.119.116";  // PC's IP  // TODO: ***CHANGE THIS**** is there an automated way
 const int udpPort = 5005;                 // Port on PC 
@@ -30,6 +30,38 @@ void setup() {
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   Serial.print("Connecting to Wi-Fi");
+
+
+  Serial.println("scan start");
+
+  // WiFi.scanNetworks will return the number of networks found
+  int n = WiFi.scanNetworks();
+  Serial.println("scan done");
+  if (n == 0) {
+      Serial.println("no networks found");
+  } else {
+    Serial.print(n);
+    Serial.println(" networks found");
+    for (int i = 0; i < n; ++i) {
+      // Print SSID and RSSI for each network found
+      Serial.print(i + 1);
+      Serial.print(": ");
+      Serial.print(WiFi.SSID(i));
+      Serial.print(" (");
+      Serial.print(WiFi.RSSI(i));
+      Serial.print(")");
+      Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
+      delay(10);
+    }
+  }
+  Serial.println("");
+
+  // Wait a bit before scanning again
+  delay(5000);
+
+
+
+
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -60,6 +92,9 @@ void setup() {
  
 void loop() {
   // // Set values to send
+
+
+
   sensors_event_t a, g, t;
   mpu.getEvent(&a, &g, &t);
   if (espmacAddress == "28:05:A5:32:DE:D0"){
